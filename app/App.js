d6582bae12d65a298bbdc9fc3b7fa17db9d578f8
Manuel Novoa <novoamanuel@ymail.com>
2024-12-11 09:08:28 -0600

    Updated readme

--- app/App.js
+++ app/App.js
@@ -1,16 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LinearGradient
+        style={styles.layout}
+      />
+      <Text>J's Tint and Car Audio</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  layout:{
+    top: 0,
+    left: 0,
+    right: 0,
+    height: '100%'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
