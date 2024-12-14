d79f66f24579ffeabd8191713bdaadaeab4f834d
Manuel Novoa <novoamanuel@ymail.com>
2024-12-14 15:14:47 -0600

    Added images and routing features.

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,10 +1,38 @@
-import { View, Image } from 'react-native';
+import { View, Image, Text, Animated, StyleSheet  } from 'react-native';
+import {useState, useCallback, useEffect, Fragment, useRef} from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Drawer } from 'expo-router/drawer';
 import { Link, Tabs } from 'expo-router';
 
+import { FloatCTA } from '../components/global';
+
 
 export default function About() {
+  const glowAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    // Start the glowing animation
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(glowAnim, {
+          toValue: 1,
+          duration: 1000, // Time to brighten
+          useNativeDriver: false,
+        }),
+        Animated.timing(glowAnim, {
+          toValue: 0,
+          duration: 1000, // Time to dim
+          useNativeDriver: false,
+        }),
+      ])
+    ).start();
+  }, [glowAnim]);
+
+  // Interpolate the background color
+  const backgroundColor = glowAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['#8B0000', '#B62025'], // From dark red to bright orange
+  });
   return (
     <>
         <GestureHandlerRootView style={{ flex: 1 }}>
@@ -68,7 +96,31 @@ export default function About() {
               />
             </Drawer>
         </GestureHandlerRootView>
+        <Animated.View style={[ styles.callButtonContainer, { backgroundColor }]}>
+          <FloatCTA/>
+        </Animated.View>
         {/* <Tabs /> */}
     </>
   );
 }
+
+
+const styles = StyleSheet.create({
+  callButtonContainer: {
+    position: 'absolute',
+    bottom: 0, // Distance from the bottom of the screen
+    // backgroundColor: '#B62025', // Customize the button color
+    // padding: 15,
+    // borderRadius: 5, // Makes the button circular
+    // shadowColor: '#000',
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 0.7,
+    // shadowRadius: 5,
+    // elevation: 5, // Adds shadow for Android
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+
+},
+})
--- app/app/index.tsx
+++ app/app/index.tsx
@@ -5,7 +5,6 @@ import { Link, Tabs} from 'expo-router';
 
 import globalStyles from "../assets/styles";
 import {LogoSection} from "../components/main-logo-section";
-import { FloatCTA } from '../components/global';
 
 import { useEvent } from 'expo';
 import { useVideoPlayer, VideoView } from 'expo-video';
@@ -22,7 +21,6 @@ export default function Page() {
   ]);
   const [isVisible, setIsVisible] = useState(false); // State to track button visibility
   const fadeAnim = useRef(new Animated.Value(0)).current; // Animation value for opacity
-  const glowAnim = useRef(new Animated.Value(0)).current;
 
 
 
@@ -37,32 +35,6 @@ export default function Page() {
 
   const { isPlaying } = useEvent(playerOne, 'playingChange', { isPlaying: playerOne.playing });
 
-
-
-
-  useEffect(() => {
-    // Start the glowing animation
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(glowAnim, {
-          toValue: 1,
-          duration: 1000, // Time to brighten
-          useNativeDriver: false,
-        }),
-        Animated.timing(glowAnim, {
-          toValue: 0,
-          duration: 1000, // Time to dim
-          useNativeDriver: false,
-        }),
-      ])
-    ).start();
-  }, [glowAnim]);
-
-  // Interpolate the background color
-  const backgroundColor = glowAnim.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['#8B0000', '#B62025'], // From dark red to bright orange
-  });
 
 
 
@@ -132,13 +104,12 @@ export default function Page() {
           <Text style={styles.copy}><Text style={styles.highlight}>Shield Your Vehicle from the Elements:</Text> Our PPF provides a durable, high-gloss barrier against rocks, road debris, bug splatter, and other environmental hazards, ensuring your paint stays pristine.</Text>
           <Text style={styles.copy}><Text style={styles.highlight}>Preserves Resale Value:</Text> Protecting your car's paint with PPF can significantly increase its resale value by maintaining a flawless exterior. Potential buyers will appreciate the care taken to protect the vehicle.</Text>
           <VideoView style={styles.video} player={playerOne} allowsFullscreen allowsPictureInPicture />
-          <Text style={styles.copy}>Driven by a passion for automotive excellence, we are committed to enhancing your driving experience through precision craftsmanship, innovative solutions, and personalized service. Our mission is to inspire confidence and satisfaction in every customer, ensuring that your journey with us is nothing short of exceptional.</Text>
+          <Text style={styles.copy}><Text style={styles.highlight}>Unobtrusive Protection:</Text> Our high-quality PPF is virtually invisible once applied, maintaining the original aesthetics of your vehicle. Enjoy peace of mind knowing your car is protected without altering its appearance.</Text>
+          <Text style={styles.copy}><Text style={styles.highlight}>Durable and Reliable:</Text> Engineered with cutting-edge technology, our PPF is designed to withstand daily driving and harsh conditions. It provides long-lasting protection, ensuring your vehicle stays looking newer for longer.</Text>
+          <Text style={styles.copy}><Text style={styles.highlight}>Easy Maintenance:</Text> PPF makes cleaning easier by repelling dirt, water, and contaminants. Say goodbye to frequent waxing and polishing—maintaining your car's shine is effortless with our protective film.</Text>
+          <Text style={styles.copy}><Text style={styles.highlight}>Tailored Solutions:</Text> We offer custom-fit PPF solutions for a wide range of vehicles, ensuring precise coverage and optimal protection. Whether you drive a sedan, SUV, or sports car, we have the right solution for you.</Text>
+          <View style={{paddingVertical: 70}}></View>
       </ScrollView>
-      {/* {isVisible && ( */}
-      <Animated.View style={[ styles.callButtonContainer, { backgroundColor }]}>
-      <FloatCTA/>
-      </Animated.View>
-      {/* )} */}
     </ScrollView>
   );
 }
